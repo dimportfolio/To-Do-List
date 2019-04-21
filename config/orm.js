4277e1f0ffc847eca0d113884fc7079b9b873c2f
dimprofile <danielimbsc@gmail.com>
2019-04-21 15:05:52 -0400

    created the orm.js file and added basic querys to create read and delete. have not tested the functionality yet

--- config/orm.js
+++ config/orm.js
@@ -7,8 +7,24 @@ const connection = require("connection");
 var orm = {
     create: function(){
         connection.query("INSERT INTO tasks SET toDo = ?", [], function(err,data){
-            if(err) throw err;
+            if (err) throw err;
             console.log("this is the create function logging " + data);
+        })
+    },
+
+    read: function(){
+        connection.query("SELECT * FROM tasks", function(err,data){
+            if (err) throw err;
+            console.log("this is the read function logging " + data);
+        })
+    },
+
+    delete: function(){
+        connection.query("DELETE FROM tasks WHERE toDo = ?", [], function(err,data){
+            if (err) throw err;
+            console.log("this is the delete function logging " + data);
         })
     }
 }
+
+module.exports = orm;
