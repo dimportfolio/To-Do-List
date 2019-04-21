dbb19e518a6e94c83ff66afe04518c55395eb6e4
dimprofile <danielimbsc@gmail.com>
2019-04-21 15:27:50 -0400

    created the server.js file, this allows us to start up our server using express, express handlebars, and routes which are in our controller.js file

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const orm = require("orm");
+const routes = require("./controller/controller.js");
+
+//??
+let PORT = process.env.PORT || 8080;
 
 let app = express();
 
+//allows us to serve static files like images, css and js
 app.use(express.static("public"));
+
+// allows us to parse request data?
+app.use(express.urlencoded( { extended : true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout : "main" }));
+app.set("view engine", "handlebars");
+
+//allows us to use the controller.js with our routes
+app.use(routes);
+
+app.listen(PORT, function(){
+    if(err)throw err;
+    console.log("Server listening on http://localhost:" + PORT);
+})
