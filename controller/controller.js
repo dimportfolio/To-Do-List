fb50cbdd041d14bdecc59bd08df3c93f6d74831f
dimprofile <danielimbsc@gmail.com>
2019-04-23 16:35:33 -0400

    added update and delete queries into orm.js file. Also created the functions in the model.js folder that allows for input to interact with databse. Completed the controller.js file that creates the routes

---
+++ controller/controller.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const router = express.Router();
+
+const model = require("../model/model.js");
+
+router.get("/", function(req, res){
+    model.all(function(result) {
+        var object = {
+            tasks: result
+        }
+        console.log(object);
+        res.render("index", object);
+    });
+});
+
+router.post("/api/tasks", function(req, res){
+    model.create([
+        "task", "status" 
+    ], [
+        req.body.name, req.body.status
+    ], function(result){
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/tasks/:id", function(req, res) {
+    var condition = `id = ${req.params.id}`
+
+    console.log("condition", condition);
+
+    model.update({
+        status: req.body.status
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
+    });
+});
+
+router.delete("/api/tasks/:id", function(req, res) {
+    var condition = `id = ${req.params.id}`
+  
+    model.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
--- model/model.js
+++ model/model.js
@@ -1,10 +1,27 @@
 const orm = require("../config/orm.js");
 
 let model = {
-    create: function(selector,cb){
-        orm.create(selector,function(res){
+    create: function(cols, vals, cb){
+        orm.create("todo", cols, vals, function(res){
+            cb(res)
+        });
+    },
+    read: function(cb){
+        orm.read("todo", function(res){
+            cb(res)
+        });
+    },
+    update: function(objColVals, condition, cb){
+        orm.update("todo", objColVals, condition, function(res){
+            cb(res)
+        });
+    },
+    delete: function(condition, cb){
+        orm.delete("todo", condition, function(res){
             cb(res)
         });
     },
 
 }
+
+module.exports = model;
